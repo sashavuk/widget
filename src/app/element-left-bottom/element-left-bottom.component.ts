ca066499b5c612a5ece8d388b030e26c922b0040
A. Kryukov <alex@kryukov.spb.ru>
2016-10-26 02:18:55 +0300

    added ngFor directive

--- src/app/element-left-bottom/element-left-bottom.component.ts
+++ src/app/element-left-bottom/element-left-bottom.component.ts
@@ -7,15 +7,17 @@ import {DataForWidget } from '../data-for-widget';
   styleUrls: ['./element-left-bottom.component.css']
 })
 export class ElementLeftBottomComponent implements OnInit {
+	selected = 'Hotel Two';
 	private my_tel;
-	//private data;
-
+	private my_places:string[] = ['Hotel','Fishing','Tours'];
+	private my_hotels:string[] = ['Hotel One','Hotel Two','Hotel Three'];
   constructor(private dataforwidget:DataForWidget) {
-
- 	
+	
   this.my_tel = dataforwidget.tel_to_send;	
   }
-
+  onSelectedChange(event){
+	console.info('event = ', event);
+  }
 
   ngOnInit() {
   }

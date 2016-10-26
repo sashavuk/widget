3e12732ba7109faf7e59421a5c7b04c45d1d2cc2
A. Kryukov <alex@kryukov.spb.ru>
2016-10-26 22:14:48 +0300

    added event

--- src/app/data-for-widget.ts
+++ src/app/data-for-widget.ts
@@ -1,11 +1,11 @@
 export class DataForWidget {
-//	public data = {
-//		"tel":"+123 45 67"
-//	}
-public tel_to_send:string = "12345";
-public temp_to_send:string = "+20";
-public water_to_send:string = "+25";
-public followers_to_send:number = 2850;
-public following_to_send:number = 675;
+	public places_to_send:string[] =  ['Экскурсии','Театры','Музеи'];
+	public temp_to_send:string = "+20";
+	public water_to_send:string = "+25";
+	public followers_to_send:number = 2850;
+	public following_to_send:number = 675;
+	public HotelOne:string[] = ['Звезда', 'Address One 1','Address Two 1','111111'];
+	public HotelTwo:string[] = ['Прибой', 'Address One 2','Address Two 2','222222'];
+	public HotelThree:string[] = ['Волна', 'Address One 3','Address Two 3','333333'];
 
 }
--- src/app/element-left-bottom/element-left-bottom.component.ts
+++ src/app/element-left-bottom/element-left-bottom.component.ts
@@ -7,17 +7,43 @@ import {DataForWidget } from '../data-for-widget';
   styleUrls: ['./element-left-bottom.component.css']
 })
 export class ElementLeftBottomComponent implements OnInit {
-	selected = 'Hotel Two';
+//	selected = 'Hotel One';
+	private selected;
+	//private dataforwidget:DataForWidget;
+	private resortName;
+	private Address1;
+	private Address2;
 	private my_tel;
-	private my_places:string[] = ['Hotel','Fishing','Tours'];
-	private my_hotels:string[] = ['Hotel One','Hotel Two','Hotel Three'];
+	private my_places:string[];
+	private my_hotels:string[];
   constructor(private dataforwidget:DataForWidget) {
-	
-  this.my_tel = dataforwidget.tel_to_send;	
+	   this.selected = dataforwidget.HotelOne[0];
+	   this.my_hotels = [dataforwidget.HotelOne[0],dataforwidget.HotelTwo[0],dataforwidget.HotelThree[0]];
+	   this.my_places = dataforwidget.places_to_send; 
+        switch(this.selected){
+			 case this.my_hotels[0]: [this.resortName,this.Address1,this.Address2,this.my_tel] = dataforwidget.HotelOne;
+		//console.info('resortName',this.resortName);
+		break;
+				case this.my_hotels[1]: [this.resortName,this.Address1,this.Address2,this.my_tel] = dataforwidget.HotelTwo;
+	//	console.info('resortName',this.resortName);
+		break;
+				case this.my_hotels[2]: [this.resortName,this.Address1,this.Address2,this.my_tel] = dataforwidget.HotelThree;
+	//	console.info('resortName',this.resortName);
+	  }
   }
   onSelectedChange(event){
-	console.info('event = ', event);
-  }
+	console.info('event = ', event,this.dataforwidget);
+	        switch(event){
+			 case this.my_hotels[0]: [this.resortName,this.Address1,this.Address2,this.my_tel] = this.dataforwidget.HotelOne;
+		//console.info('resortName',this.resortName);
+		break;
+				case this.my_hotels[1]: [this.resortName,this.Address1,this.Address2,this.my_tel] = this.dataforwidget.HotelTwo;
+	//	console.info('resortName',this.resortName);
+		break;
+				case this.my_hotels[2]: [this.resortName,this.Address1,this.Address2,this.my_tel] = this.dataforwidget.HotelThree;
+	//	console.info('resortName',this.resortName)
+    }/**/
+ }
 
   ngOnInit() {
   }
